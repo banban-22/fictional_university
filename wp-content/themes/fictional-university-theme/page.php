cdc89ed9b559b3d40d6c001339fd3be1561972b3
a1ya1k2a9@gmail.com <a1ya1k2a9@gmail.com>
2023-11-22 08:21:24 -0800

    added customized plugin

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -9,6 +9,7 @@ while (have_posts()) {
   <div class="container container--narrow page-section">
     <?php
     $theParent = wp_get_post_parent_id(get_the_ID());
+
     if ($theParent) { ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
@@ -30,6 +31,7 @@ while (have_posts()) {
         'child_of' => get_the_ID()
       )
     );
+
     if ($theParent or $testArray) { ?>
 
       <div class="page-links">
